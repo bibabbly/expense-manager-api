ecf2b470c1a1a47eea9065d3a8b11876c5628393
bibabbly <bizimungu2004@gmail.com>
2025-05-23 08:59:05 +0200

    Fix: add CORS for Vercel frontend

--- src/main/java/com/theo/expense_manager/config/WebConfig.java
+++ src/main/java/com/theo/expense_manager/config/WebConfig.java
@@ -13,7 +13,8 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://localhost:3000",
+                                "https://expense-manager-client-tan.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
